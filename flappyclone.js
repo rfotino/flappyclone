5c406859624e33b35f63893d84a8fff533dd4cab
Robert Fotino <robert.fotino@gmail.com>
2014-02-16 18:05:38 -0800

    Added collision detection and a losing screen that tells you your score, plus the ability to restart.

--- flappyclone.js
+++ flappyclone.js
@@ -1,31 +1,8 @@
 document.addEventListener("DOMContentLoaded", flappycloneInit, false);
-var canvas = document.createElement("canvas");
-var bird = {
-    position: {
-	x: 100,
-	y: 320
-    },
-    velocity: 0,
-    maxVelocity: 30,
-    acceleration: 2,
-    update: function() {
-	this.position.y += this.velocity;
-	this.velocity = Math.min(this.velocity + this.acceleration, this.maxVelocity);
-    },
-    draw: function(context) {
-	context.fillStyle = "#000000";
-	context.beginPath();
-	context.arc(this.position.x, this.position.y, 10, 0, 2*Math.PI);
-	context.fill();
-    },
-    jump: function() {
-	this.velocity = -20;
-    }
-};
-var pipes = new Array();
-var score = 0;
+var interval;
 
 function flappycloneInit() {
+    canvas = document.createElement("canvas");
     canvas.setAttribute("width", "960px");
     canvas.setAttribute("height", "640px");
     canvas.setAttribute("tabindex", 1);
@@ -38,13 +15,66 @@ function flappycloneInit() {
     canvas.onkeyup = flappycloneKeyUp;
     context = canvas.getContext("2d");
     
-    flappycloneUpdate();
+    flappycloneStart();
+}
+
+function flappycloneStart() {
+    clearInterval(interval);
+    
+    bird = {
+	position: {
+	    x: 200,
+	    y: 320
+	},
+	velocity: 0,
+	maxVelocity: 30,
+	acceleration: 2,
+	dead: false,
+	update: function() {
+	    this.position.y += this.velocity;
+	    this.velocity = Math.min(this.velocity + this.acceleration, this.maxVelocity);
+
+	    //collision
+	    if (this.position.y > canvas.height - 25) {
+		this.die();
+	    } else {
+		for (var i = 0; i < pipes.length; i++) {
+		    var pipe = pipes[i];
+		    if (this.position.x >= pipe.position && this.position.x <= pipe.position + pipe.width) {
+			if (this.position.y < pipe.gapPosition - 75 || this.position.y > pipe.gapPosition + 75) {
+			    this.die();
+			}
+		    }
+		}
+	    }
+	},
+	draw: function(context) {
+	    context.fillStyle = "#000000";
+	    context.beginPath();
+	    context.arc(this.position.x, this.position.y, 10, 0, 2*Math.PI);
+	    context.fill();
+	},
+	jump: function() {
+	    this.velocity = -20;
+	},
+	die: function() {
+	    clearInterval(interval);
+	    this.dead = true;
+	}
+    };
+    score = 0;
+    pipes = new Array();
+    
+    flappycloneDraw();
+    interval = setInterval(flappycloneUpdate, 1000/30);
 }
 
 var spacedown = false;
 function flappycloneKeyDown(e) {
     var key = e.which || e.keyCode;
-    if (key === 32 && !spacedown) {
+    if (key === 13) {
+	flappycloneStart();	
+    } else if (key === 32 && !spacedown) {
 	bird.jump();
 	spacedown = true;
     }
@@ -57,7 +87,8 @@ function flappycloneKeyUp(e) {
 }
 
 function flappycloneUpdate() {
-    bird.update();
+    var context = canvas.getContext("2d");
+    bird.update(context);
     if (pipes.length === 0 || pipes[pipes.length-1].position < canvas.width - 250) {
 	pipes[pipes.length] = {
 	    width: 50,
@@ -79,7 +110,7 @@ function flappycloneUpdate() {
 	    pipes.splice(i, 1);
 	} else {
 	    pipes[i].update();
-	    if (!pipes[i].addedPoints && pipes[i].position < bird.position.x) {
+	    if (!pipes[i].addedPoints && pipes[i].position + pipes[i].width < bird.position.x) {
 		score++;
 		pipes[i].addedPoints = true;
 	    }
@@ -91,19 +122,28 @@ function flappycloneUpdate() {
 function flappycloneDraw() {
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
-    bird.draw(context);
-    for (var i = 0; i < pipes.length; i++) {
-	pipes[i].draw(context);
-    }
     
-    //draw score
-    context.fillStyle = "#999";
-    context.font = "42px sans-serif";
-    context.fillText(score, 50, 50);
+    if (!bird.dead) {
+    bird.draw(context);
+	for (var i = 0; i < pipes.length; i++) {
+	    pipes[i].draw(context);
+	}
+
+	//draw score
+	context.fillStyle = "#999";
+	context.font = "42px sans-serif";
+	context.fillText(score, 50, 50);
+    } else {
+	context.fillStyle = "#000";
+	context.font = "64px sans-serif";
+	context.fillText("SCORE: " + score, 325, 300);
+	
+	context.fillStyle = "#333";
+	context.font = "24px sans-serif";
+	context.fillText("PRESS ENTER TO PLAY AGAIN", 310, 365);
+    }
     
     //draw floor
     context.fillStyle = "#000";
     context.fillRect(0, canvas.height-25, canvas.width, 25);
-    
-    setTimeout(flappycloneUpdate, 1000/30);
 }
