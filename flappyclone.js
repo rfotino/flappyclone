e9b54dc84eb513fa833aeea3a4ab6f80aa3acb8f
Robert Fotino <robert.fotino@gmail.com>
2014-02-16 16:49:05 -0800

    Added input so that the bird jumps via the space bar.

--- flappyclone.js
+++ flappyclone.js
@@ -4,7 +4,7 @@ var bird = {
     position: 320,
     velocity: 0,
     maxVelocity: 30,
-    acceleration: 3,
+    acceleration: 2,
     update: function() {
 	this.position += this.velocity;
 	this.velocity = Math.min(this.velocity + this.acceleration, this.maxVelocity);
@@ -14,19 +14,41 @@ var bird = {
 	context.beginPath();
 	context.arc(100, this.position, 10, 0, 2*Math.PI);
 	context.fill();
+    },
+    jump: function() {
+	this.velocity = -20;
     }
 };
 
 function flappycloneInit() {
     canvas.setAttribute("width", "960px");
     canvas.setAttribute("height", "640px");
+    canvas.setAttribute("tabindex", 1);
     canvas.style.display = "block";
     canvas.style.margin = "0 auto";
+    canvas.style.outline = "none";
     document.body.appendChild(canvas);
     canvas.focus();
+    canvas.onkeydown = flappycloneKeyDown;
+    canvas.onkeyup = flappycloneKeyUp;
     context = canvas.getContext("2d");
     
     flappycloneUpdate();
+}
+
+var spacedown = false;
+function flappycloneKeyDown(e) {
+    var key = e.which || e.keyCode;
+    if (key === 32 && !spacedown) {
+	bird.jump();
+	spacedown = true;
+    }
+}
+function flappycloneKeyUp(e) {
+    var key = e.which || e.keyCode;
+    if (key === 32) {
+	spacedown = false;
+    }
 }
 
 function flappycloneUpdate() {
